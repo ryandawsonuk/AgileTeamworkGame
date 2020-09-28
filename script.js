81c725f36ec4047a23b441668a9256d4c55436f2
ryandawsonuk <rd@seldon.io>
2020-09-28 18:46:22 +0100

    more situations

--- script.js
+++ script.js
@@ -94,11 +94,29 @@ $(document).ready(function() {
             {
 	            situation: "We need the team to work the weekend. Just this once.",
 	            perspectives:["Why is this so urgent?",
-                "Weekend work will wear the team down if we do it regularly.",
+                "Weekend work will wear the team down if we do it regularly",
                 "How did this get missed in previous plans?",
                 "We need to ask individuals without putting them on the spot",
                 "How do we plan better in future?"],
                 situationNum: "Situation 2"
+            },
+            {
+	            situation: "What do you mean it won't scale to that many users?",
+	            perspectives:["We haven't tested it, that wasn't listed as a non-functional requirement",
+                "We can put in a story to assess what it will scale to",
+                "Is that the number it needs to scale to? When would we expect that load?",
+                "Can we influence how much traffic will come when? Could we make the increase more staggered?",
+                "Is there a quick way to reduce the risk of failure from increased traffic?"],
+                situationNum: "Situation 3"
+            },
+            {
+	            situation: "We would normally do more testing but we need to get this feature out ASAP",
+	            perspectives:["This could be tech debt that we'll really struggle to pay down",
+                "As someone who cares about testing, I can't sign this off",
+                "What is ASAP and what's the impact of waiting?",
+                "What could go wrong with this in production? What would the impacts be?",
+                "Can we do a risk assessment?"],
+                situationNum: "Situation 4"
             }
         ];
         
